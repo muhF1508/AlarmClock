427e78ebc429a512098a486af283acaa75ba24c4
muhF1508 <firdausbarra08@gmail.com>
2018-11-12 11:02:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mbarrafirdaus/alarmclock/Splash.java
@@ -0,0 +1,27 @@
+package com.example.mbarrafirdaus.alarmclock;
+
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, AlarmActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000L);
+    }
+}
